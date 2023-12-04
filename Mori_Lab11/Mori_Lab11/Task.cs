318adac23dcaac60389cf44a91ea9620dc0bea34
Max54611 <magno.mori@tecsup.edu.pe>
2023-12-03 21:52:05 -0500

    Agregar archivos de proyecto.

---
+++ Mori_Lab11/Mori_Lab11/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mori_Lab11
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task() { }
+
+        public Task(int Id, string Title, string Description, bool IsCompleted) 
+        { 
+            this.Id = Id;
+            this.Title = Title;
+            this.Description = Description;
+            this.IsCompleted = IsCompleted;
+        }
+    }
+}
---
+++ Mori_Lab11/Mori_Lab11/TaskViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Mori_Lab11
+{
+    public class TaskViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Task> Tasks { get; set; } = new ObservableCollection<Task>();
+
+        private string newTaskTitle;
+        public string NewTaskTitle
+        {
+            get { return newTaskTitle; }
+            set
+            {
+                if (newTaskTitle != value)
+                {
+                    newTaskTitle = value;
+                    OnPropertyChanged(nameof(NewTaskTitle));
+                }
+            }
+        }
+
+        private string newTaskDescription;
+        public string NewTaskDescription
+        {
+            get { return newTaskDescription; }
+            set
+            {
+                if (newTaskDescription != value)
+                {
+                    newTaskDescription = value;
+                    OnPropertyChanged(nameof(NewTaskDescription));
+                }
+            }
+        }
+
+        public ICommand AddTaskCommand => new Command(Insertar);
+
+        private void Insertar()
+        {
+            Task newTask = new Task
+            {
+                Title = NewTaskTitle,
+                Description = NewTaskDescription
+            };
+
+            Tasks.Add(newTask);
+            OnPropertyChanged(nameof(Tasks));
+
+            // Limpiar los campos después de agregar la tarea
+            NewTaskTitle = string.Empty;
+            NewTaskDescription = string.Empty;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
